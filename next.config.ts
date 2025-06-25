0878270e650b95bfebdc77c5c9690689b27c3277
Walton Wang <yuwaltonwang@gmail.com>
2025-06-24 21:41:29 -0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: process.env.NODE_ENV === "development" ? ".next/dev" : ".next/build",
 };
 
 export default nextConfig;
